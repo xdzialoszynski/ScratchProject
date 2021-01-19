df19a3858b44f71cf0a923677cce7821a7743563
xdzialoszynski <xdzialoszynski@gmail.com>
2021-01-19 11:24:07 +0100

    Create CheckDigit.java

---
+++ CheckDigit.java
@@ -0,0 +1,33 @@
+package com.braincube.java8;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckDigit {
+
+    public static void main(String[] args) {
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new FileReader(new File(args[0])));
+            String line = null;
+            int i = 0;
+            int result = -1;
+            line = in.readLine();
+            int n = Integer.valueOf(line);
+
+
+            result = line.chars().map(x->Character.getNumericValue(x)).reduce(0, (x,y)->x+y);
+            while (String.valueOf(result).length()>1){
+                line = String.valueOf(result);
+                result = line.chars().map(x->Character.getNumericValue(x)).reduce(0, (x,y)->x+y);
+            }
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
